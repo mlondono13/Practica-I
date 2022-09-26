20b16416379eac27504f40c6ed003aef6ad26053
mlondono13 <104228951+mlondono13@users.noreply.github.com>
2022-09-26 11:11:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@
 #include "colors.h"
 #include "Node.h"
 #include "LinkedList.cpp"
+#include <string.h>
 using namespace std;
+
+string aMayuscula(string cadena) {
+  for (int i = 0; i < cadena.length(); i++)
+    cadena[i] = toupper(cadena[i]);
+    return cadena;
+}
 
 Color::Modifier green(Color::FG_GREEN);
 Color::Modifier yellow(Color::FG_YELLOW);
 Color::Modifier def(Color::FG_DEFAULT);
 
-LinkedList convertirLista(string palabra){
-  LinkedList l1;
+LinkedList* convertirLista(string palabra){
+  LinkedList* l1;
   for(int i = palabra.length()-1; i>=0; i--){
-    l1.add_at_head(palabra[i]);
+    l1->add_at_head(palabra[i]);
   }
   return l1;
 }
@@ -31,11 +38,11 @@ char convertirString(string s){
 return 0;
 }
 
-string diccionario[] = {"COSTA", "GORRA", "BOLSA", "BALON", "LISTA", "PERRO", "ARROZ", "PALMA", "ARBOL", "PLAYA"};
+string diccionario[] = {"COSTA", "GORRA", "BOLSA", "BALON", "LISTA", "PERRO", "ARROZ", "PALMA", "ARBOL", "PLAYA", "GATO"};
 
 int obtenerNumeroRandom(){
   srand(time(NULL));
-  int numRandom = rand()%10;
+  int numRandom = rand()%11;
   return numRandom;
 }
 
@@ -45,7 +52,7 @@ string palabraAleatoria(string diccionario[]){//arroja una palabra aleatoria
   return palabraAleatoria ;
 }
 
-Node* compararListas(LinkedList* lista1, LinkedList* lista2){
+void compararListas(LinkedList* lista1, LinkedList* lista2){
   Node* n1 = lista1->head;
   Node* n2 = lista2->head;
   Node* equal_node;
@@ -59,11 +66,9 @@ Node* compararListas(LinkedList* lista1, LinkedList* lista2){
           if(i==j){
             equal_node = n2;
             cout<<green<<equal_node->value<<endl;
-            return equal_node;
           }else{
             node_in_llp = n2;
             cout<<yellow<<n2->value<<endl;
-            return node_in_llp;
           }
         }else{
           n1 = n1->next;
@@ -77,31 +82,36 @@ Node* compararListas(LinkedList* lista1, LinkedList* lista2){
     }
   not_in_llp = n2;
   cout<<def<<n2->value<<endl;
-  return not_in_llp;
 }
 
 int main(){
 string solucion = palabraAleatoria(diccionario);
-LinkedList LLP = convertirLista(solucion);  
+LinkedList *LLP = convertirLista(solucion);  
 cout << "Bienvenido a Wordle" << endl;
 cout << "Inserta 1 para jugar o 0 para salir" << endl;
 int opcion;
 cin >> opcion;
-LinkedList LLI;
+LinkedList *LLI;
   if(opcion == 1){ //se ejecuta el programa
-    cout << "La palabra a adivinar tiene 5 letras..." << endl;
+    cout << "La palabra a adivinar tiene" << solucion.length()<< " letras..." << endl;
     cout << "Tienes 6 intentos para adivinarla" << endl;
     string palabra;
     for(int i=0; i<6 ; i++){//intentos del usuario
       cin >> palabra;
-      LLI = convertirLista(palabra);
-      if(solucion == palabra){
-        cout << "Ganaste" << endl;
+      palabra = aMayuscula(palabra);
+      if(palabra.length() == solucion.length()){
+        LLI = convertirLista(palabra);
+        if(solucion == palabra){
+          cout << green << palabra << endl;
+          cout << "Ganaste" << endl;
+        }else{
+          compararListas(LLP, LLI);
+        }
       }else{
-        compararListas(LLP, LLI);
+        cout << "¡Ingresa una palabra con " << solucion.length() << " letras!" << endl;
       }
     }
   }else if(opcion == 0){
-    break;
+    exit(opcion);
   }
 }
