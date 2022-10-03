21f71de3ba2e157547fc0f31617eace0a58ee657
mlondono13 <104228951+mlondono13@users.noreply.github.com>
2022-10-02 22:22:32 -0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,13 +1,14 @@
-#pragma once
 #include <iostream>
 #include <string>
 using namespace std;
 class Node {
 public:
   char value;
-  Node *next;
+  string color;
+  Node* next;
   Node(char value) {
     this->value = value;
     this->next = NULL;
+    this->color = "default";
   }
 };
