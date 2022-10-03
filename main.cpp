12af2e5a1c93742ad4ace8fd14fbf6a60115ba48
mlondono13 <104228951+mlondono13@users.noreply.github.com>
2022-10-02 22:23:16 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,26 +5,30 @@
 #include <bits/stdc++.h>
 #include "colors.h"
 #include "Node.h"
-#include "LinkedList.cpp"
 #include <string.h>
+#include <list>
+#include <iterator>
 using namespace std;
 
 string aMayuscula(string cadena) {
-  for (int i = 0; i < cadena.length(); i++)
+  for(int i = 0; i < cadena.length(); i++){
     cadena[i] = toupper(cadena[i]);
-    return cadena;
+  }
+  return cadena;
 }
 
 Color::Modifier green(Color::FG_GREEN);
 Color::Modifier yellow(Color::FG_YELLOW);
 Color::Modifier def(Color::FG_DEFAULT);
 
-LinkedList* convertirLista(string palabra){
-  LinkedList* l1;
-  for(int i = palabra.length()-1; i>=0; i--){
-    l1->add_at_head(palabra[i]);
+Node* convertirLista(string palabra){
+  Node* head = new Node(palabra[0]);
+  Node* temp = head;
+  for(int i = 1; i<palabra.size(); i++){
+    temp->next = new Node(palabra[i]);
+    temp = temp->next;
   }
-  return l1;
+  return head;
 }
 
 string diccionario[] = {"COSTA", "GORRA", "BOLSA", "BALON", "LISTA", "PERRO", "ARROZ", "PALMA", "ARBOL", "PLAYA", "GATO"};
@@ -41,66 +45,103 @@ string palabraAleatoria(string diccionario[]){//arroja una palabra aleatoria
   return palabraAleatoria ;
 }
 
-void compararListas(LinkedList* lista1, LinkedList* lista2){
-  Node* n1 = lista1->head;
-  Node* n2 = lista2->head;
-  Node* equal_node;
-  Node* node_in_llp;
-  Node* not_in_llp;
-  int i=0;
-  int j=0;
-  while(n2 != NULL){
-      while(n1 != NULL){
-        if(n1->value == n2->value){
-          if(i==j){
-            equal_node = n2;
-            cout<<green<<equal_node->value<<endl;
-          }else{
-            node_in_llp = n2;
-            cout<<yellow<<n2->value<<endl;
-          }
-        }else{
-          n1 = n1->next;
-          i++;
-        }
-      }
-      n1 = lista1->head;
-      i = 0;
-      n2 = n2->next;
-      j++;
-    }
-  not_in_llp = n2;
-  cout<<def<<n2->value<<endl;
+void reset(Node* LLI){
+  while(LLI != NULL){
+    LLI->color = "default";
+    LLI = LLI->next;
+  }
 }
 
+void comparar(Node* LLPhead, Node* LLIhead){
+  Node* llp = LLPhead;
+  Node* lli = LLIhead;
+  int i = 0;
+  int j = 0;
+  while(lli != NULL){
+    i = 0;
+    llp = llp;
+    while(llp != NULL){
+      if(lli->value == llp->value){
+        if (j==i){
+          lli->color = "green";
+          break;
+        }else{
+          lli->color = "yellow";
+        }
+      }
+      llp = llp->next;
+      i++;
+    }
+    lli = lli->next;
+    j++;
+  }
+}
+
+bool imprimirColor(Node* LLI, Node* LLP){
+  Node* lli = LLI;
+  while(lli != NULL){
+    if(lli->color == "green"){
+      cout<<green<<lli->value<<def;
+    }else if (lli->color == "yellow"){
+      cout<<yellow<<lli->value<<def;
+    }else{
+      cout<<lli->value;
+    }
+    lli = lli->next;
+  }
+  if(LLI == LLP){
+    cout << "Ganaste!" << endl;
+    return true;
+  }else{
+    return false;
+  }
+}
+/*bool verificarGanar(Node* LLP, Node* LLI){
+  Node* llp = LLP;
+  Node* lli = LLI;
+  
+}*/
+
+
 int main(){
-string solucion = palabraAleatoria(diccionario);
-LinkedList *LLP = convertirLista(solucion);  
-cout << "Bienvenido a Wordle" << endl;
-cout << "Inserta 1 para jugar o 0 para salir" << endl;
-int opcion;
-cin >> opcion;
-LinkedList *LLI;
+  string solucion = palabraAleatoria(diccionario);
+  Node* LLP = convertirLista(solucion);  
+  cout << "Bienvenido a Wordle" << endl;
+  cout << "Inserta 1 para jugar o 0 para salir" << endl;
+  int opcion;
+  cin >> opcion;
+  Node* LLI;
+  bool verificar;
   if(opcion == 1){ //se ejecuta el programa
     cout << "La palabra a adivinar tiene " << solucion.length()<< " letras..." << endl;
     cout << "Tienes 6 intentos para adivinarla" << endl;
     string palabra;
-    for(int i=0; i<6 ; i++){//intentos del usuario
+    int intentos;
+    for(intentos=0; intentos<6 ; intentos++){//intentos del usuario
+      cout<<"\nIntento "<<intentos+1<<": ";
       cin >> palabra;
       palabra = aMayuscula(palabra);
+      LLI = convertirLista(palabra);
       if(palabra.length() == solucion.length()){
-        LLI = convertirLista(palabra);
-        if(solucion == palabra){
-          cout << green << palabra << endl;
-          cout << "Ganaste" << endl;
-        }else{
-          compararListas(LLP, LLI);
+        comparar(LLP, LLI);
+        verificar = imprimirColor(LLI, LLP);
+        reset(LLI);
+        if(verificar == true){
+          break;
         }
       }else{
         cout << "¡Ingresa una palabra con " << solucion.length() << " letras!" << endl;
+        intentos--;
       }
+       
+    } if(intentos == 6){
+       cout<<"\nUps...sigue intentando"<<endl;
+       cout<<"La palabra correcta es: "<<green<<solucion<<endl;
+      } 
     }
-  }else if(opcion == 0){
-    exit(opcion);
+    
+  else if(opcion == 0){
+    cout<<"Hasta pronto!";
+    exit(opcion);  
   }
-}
+} 
